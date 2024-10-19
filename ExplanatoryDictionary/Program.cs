108df6d068328e3ce2a7c3a5d6e264c79b9ea96c
Feaxmutch <feaxmutch@gmail.com>
2024-10-19 11:57:35 +0300

    Copyed the old solution

--- ExplanatoryDictionary/Program.cs
+++ ExplanatoryDictionary/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Dictionary<string, string> capitals = new Dictionary<string, string>
+            {
+                { "Россия", "Москва" },
+                { "Англия", "Лондон" },
+                { "Германия", "Берлин"},
+                { "Франция", "Париж"},
+                { "Италия", "Рим"},
+                { "Испания", "Мадрид"}
+            };
+
+            Console.WriteLine($"Столица: {GetCapital(capitals)}");
+            Console.ReadKey();
+        }
+
+        static string GetCapital(Dictionary<string, string> capitalsCollection)
+        {
+            string userInput = string.Empty;
+            bool isCorrectKey = false;
+
+            while (isCorrectKey == false)
+            {
+                Console.Clear();
+                Console.WriteLine("Введите страну");
+                userInput = Console.ReadLine();
+
+                if (capitalsCollection.ContainsKey(userInput))
+                {
+                    isCorrectKey = true;
+                }
+                else
+                {
+                    Console.WriteLine("Я не знаю такую страну");
+                    Console.ReadKey();
+                }
+            }
+
+            return capitalsCollection[userInput];
         }
     }
 }
